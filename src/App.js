a938377d229408504bbc70bec17cf8a9f0b58b9f
carson1011 <yamu_1011@yonsei.ac.kr>
2022-09-18 17:27:49 +0900

    Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
+import Customer from "./components/Customer";
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} let="logo"></img>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers =[
+    {
+        'id':1,
+        'image' : 'http://placeimg.com/64/64/com',
+        'name' : '홍길동',
+        'birthday':'921011',
+        'gender': '남자',
+        'job': '대학생'
+    },
+    {
+        'id':2,
+        'image' : 'http://placeimg.com/64/64/com',
+        'name' : '홍길동',
+        'birthday':'921011',
+        'gender': '남자',
+        'job': '대학생'
+    },
+    {
+        'id':3,
+        'image' : 'http://placeimg.com/64/64/com',
+        'name' : '이순신',
+        'birthday':'921011',
+        'gender': '남자',
+        'job': '대학생'
+    }
+]
+
+class App extends Component{
+    render() {
+        return(
+            <div>{
+                customers.map(c=>{
+                    return (
+                        <Customer
+                            id={c.id}
+                            image={c.image}
+                            name={c.name}
+                            birthday={c.birthday}
+                            gender={c.gender}
+                            job={c.job}
+                            />
+                    )
+                })
+            }
+            </div>
+        );
+    }
 }
+
+
 
 export default App;
