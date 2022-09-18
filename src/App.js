95ad139c2ba9e29fafd6bc801f87ca4bf9cec59b
carson1011 <yamu_1011@yonsei.ac.kr>
2022-09-18 16:11:26 +0900

    message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} let="logo"></img>
+      <h2>Let's develop management system!</h2>
+    </div>
+  );
+}
+
+export default App;
