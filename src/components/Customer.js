dc5380094513a97c176cb30d8d22197a60e251b1
carson1011 <yamu_1011@yonsei.ac.kr>
2022-09-19 20:41:47 +0900

    material ui

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -11,27 +11,6 @@ class Customer extends React.Component {
     }
 }
 
-class CustomerProfile extends React.Component {
-    render() {
-        return(
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        );
-    }
-}
 
-class CustomerInfo extends React.Component {
-    render() {
-        return(
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
 
 export default Customer;
